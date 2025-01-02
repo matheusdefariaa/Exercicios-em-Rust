039ad83b9fcf7a7f2a88008d68a25c9b5e7de0a0
matheus de faria <mattheusdefaria@proton.me>
2025-01-01 23:08:33 -0300

    calculator v1

--- calculator_v1/target/debug/deps/calculator_v1-490c34dc80fe9e2a.d
+++
@@ -1,5 +0,0 @@
-/home/matheus/Documentos/Projects/Rust/Exercicios-em-Rust/calculator_v1/target/debug/deps/calculator_v1-490c34dc80fe9e2a: src/main.rs
-
-/home/matheus/Documentos/Projects/Rust/Exercicios-em-Rust/calculator_v1/target/debug/deps/calculator_v1-490c34dc80fe9e2a.d: src/main.rs
-
-src/main.rs:
