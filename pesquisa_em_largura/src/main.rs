44e2cd3bd511f988835f7a54f31a5b22262e125e
matheus de faria <mattheusdefaria@proton.me>
2025-01-30 10:59:03 -0300

    final

--- pesquisa_em_largura/src/main.rs
+++ pesquisa_em_largura/src/main.rs
@@ -15,15 +15,37 @@ fn main() {
 }
 
 fn comeca_com_m(p: &str) -> bool {
-    if p.starts_with("M") {
-        return true
-    }
-    false
+    p.starts_with("L")
 }
 
-fn pesquisa_em_largura(nome: &str,lista: HashMap<&str,Vec<&str>>) -> String {
-    let mut fila: VecDeque<HashMap<&str,Vec<&str>>> = VecDeque::new();
-    let mut verificados: Vec<String> = Vec::new();
+fn pesquisa_em_largura(nome: &str,lista: HashMap<&str,Vec<&str>>) -> bool {
+    // let mut fila: VecDeque<HashMap<&str,Vec<&str>>> = VecDeque::new();
+    // let mut fila: VecDeque<&Vec<&str>> = VecDeque::new();
+    let mut fila: VecDeque<&str> = VecDeque::new();
+    let mut verificados: Vec<&str> = Vec::new();
 
-    "a".to_string()
+    fila.push_back(nome);
+
+    while !fila.is_empty() {
+        let p1 = fila.pop_front().unwrap();
+
+        if !verificados.contains(&p1) {
+            if comeca_com_m(p1) {
+                println!("{:?}",p1);
+                return true
+            }
+
+            verificados.push(&p1);
+
+            if let Some(amigos) = lista.get(p1) {
+                for &amigo in amigos {
+                    if !verificados.contains(&amigo) {
+                        fila.push_back(amigo);
+                    }
+                }
+            }
+        }
+    }
+
+    false
 }
