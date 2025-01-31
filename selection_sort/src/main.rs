8f11bebe555e20b744d4cd35e55bd30f53c2f02c
matheus de faria <mattheusdefaria@proton.me>
2025-01-31 12:30:26 -0300

    commit

--- selection_sort/src/main.rs
+++ selection_sort/src/main.rs
@@ -1,34 +1,32 @@
-// Selection Sort
 fn main() {
-    let mut numeros_lista: Vec<i32> = vec![6,5,4,3,2,1];
-    println!("{numeros_lista:?}");
-
-    let nova_lista = selection_sort(&mut numeros_lista);
-    println!("{nova_lista:?}");
+    let mut vec = vec![9,3,1,6,8,2,0,-1,100,-2,200,300];
+    let n = merge_sort(&mut vec);
+    println!("{:?}",n);
 }
 
-fn selection_sort(arr: &mut Vec<i32>) -> Vec<i32> {
-    let mut nova_lista_numeros: Vec<i32> = Vec::new();
 
-    for elem in 0..arr.len() {
-        let menor_index: usize = menor_numero(arr);
-        nova_lista_numeros.push(arr[menor_index]);
-        arr.remove(menor_index);
-    }
-    nova_lista_numeros
+fn menor(arr: &[i32]) -> usize {
+    let mut menor = arr[0];
+    let mut ind: usize = 0;
 
-}
-
-fn menor_numero(arr: &Vec<i32>) -> usize {
-    let mut menor: i32 = arr[0];
-    let mut menor_index = 0 as usize;
-
-    for elem in 1..arr.len() {
-        if arr[elem] < menor {
-            menor = arr[elem];
-            menor_index = elem;
+    for indx in 1..arr.len() {
+        if arr[indx] < menor {
+            menor = arr[indx];
+            ind = indx;
         }
-    };
+    }
 
-    menor_index
+    ind
+}
+
+
+fn merge_sort(arr: &mut Vec<i32>) -> Vec<i32> {
+    let mut novo_vetor: Vec<i32> = Vec::new();
+
+    for indx in 0..arr.len() {
+        let menor = menor(&arr);
+        novo_vetor.push(arr.remove(menor));
+    }
+
+    novo_vetor
 }
